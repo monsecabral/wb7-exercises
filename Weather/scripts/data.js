aa04380c7069e91420595f3f0fe6560dbddc982b
monsecabral <cabral_monse@yahoo.com>
2024-06-05 14:41:07 -0400

    exercise 2 step 2

---
+++ Weather/scripts/data.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let cities = [
+    {   name: "Roswell, GA",
+        latitude: 34.0079026,
+        longitude: -84.3183853
+    }, {
+        name: "Duluth, GA",
+        latitude: 34.0002042,
+        longitude: -84.179665
+    }, {
+       name: "Norcross, GA",
+       latitude: 33.9409016,
+       longitude: -84.2206319 
+    }
+];
